d9aa457aa981f0e8d756f5597c4fbecc2dbd7270
9KICKS <officialjoshua9@gmail.com>
2023-06-03 20:53:25 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/RegcrowApplication.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/RegcrowApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegcrowApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RegcrowApplication.class, args);
 	}
-
 }
