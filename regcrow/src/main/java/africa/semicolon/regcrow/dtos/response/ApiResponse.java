f284e09cf2ded4d8375d3b13deda0a72866ba9ad
9KICKS <officialjoshua9@gmail.com>
2023-06-03 20:52:38 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/dtos/response/ApiResponse.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/dtos/response/ApiResponse.java
@@ -1,5 +1,6 @@
 package africa.semicolon.regcrow.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Getter
@@ -7,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private String message;
     private T data;
