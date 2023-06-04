679cd288803944282ba7ef471f667e24ac67a94c
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:56:20 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/utils/ResponseUtils.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/utils/ResponseUtils.java
@@ -4,4 +4,7 @@ public class ResponseUtils {
     public static final String USER_REGISTRATION_SUCCESSFUL = "User registration successful.";
     public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully.";
     public static final String PROFILE_UPDATED_SUCCESSFULLY = "Profile updated successfully.";
+    public static final String ADD_BANK_ACCOUNT_SUCCESSFUL = "Added bank account successfully.";
+    public static final String BANK_ACCOUNT_DELETED_SUCCESSFULLY = "Bank account deleted successfully.";
+    public static final String UPDATE_BANK_ACCOUNT_DETAILS_SUCCESSFULLY = "Bank account details updated successfully.";
 }
