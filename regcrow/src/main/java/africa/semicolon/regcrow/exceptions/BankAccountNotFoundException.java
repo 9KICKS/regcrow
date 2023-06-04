0304529b84b3c10fe3713829ed5f2bb77b9020bd
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:55:14 +0100

    "updated the Regrow app"

---
+++ regcrow/src/main/java/africa/semicolon/regcrow/exceptions/BankAccountNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.regcrow.exceptions;
+
+public class BankAccountNotFoundException extends RegcrowException {
+    public BankAccountNotFoundException(String message) {
+        super(message);
+    }
+}
