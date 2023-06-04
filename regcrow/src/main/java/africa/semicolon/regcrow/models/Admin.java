b0c5b84c4784bbd476cef21e92720dec6e859c2e
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:58:47 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/models/Admin.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/models/Admin.java
@@ -1,16 +1,38 @@
 package africa.semicolon.regcrow.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "regcrow_admin")
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @OneToOne()
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private BioData bioData;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime timeCreated;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void setTimeCreated() {
+        this.timeCreated = LocalDateTime.now();
+    }
 }
