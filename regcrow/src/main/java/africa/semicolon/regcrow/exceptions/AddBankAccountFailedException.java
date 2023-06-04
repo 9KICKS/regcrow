b177f8713c135e93ab7175347e14125bab14cf47
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:54:20 +0100

    "updated the Regrow app"

---
+++ regcrow/src/main/java/africa/semicolon/regcrow/exceptions/AddBankAccountFailedException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.regcrow.exceptions;
+
+public class AddBankAccountFailedException extends RegcrowException {
+    public AddBankAccountFailedException(String message) {
+        super(message);
+    }
+}
