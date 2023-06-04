b7091390bd580055589ecc915f32deabfbc29474
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:54:55 +0100

    "updated the Regrow app"

---
+++ regcrow/src/main/java/africa/semicolon/regcrow/exceptions/AdminRegistrationFailedException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.regcrow.exceptions;
+
+public class AdminRegistrationFailedException extends RegcrowException {
+    public AdminRegistrationFailedException(String message) {
+        super(message);
+    }
+}
