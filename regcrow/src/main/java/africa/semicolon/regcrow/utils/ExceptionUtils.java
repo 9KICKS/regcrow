534e2cdcbe83e0bda99bbff26ad5a3fb1578182d
9KICKS <officialjoshua9@gmail.com>
2023-06-04 22:57:08 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/utils/ExceptionUtils.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/utils/ExceptionUtils.java
@@ -3,4 +3,6 @@ package africa.semicolon.regcrow.utils;
 public class ExceptionUtils {
     public static final String USER_REGISTRATION_FAILED = "User with email %s unable to register.";
     public static final String USER_WITH_ID_NOT_FOUND = "User with id %d not found.";
+    public static final String ADD_BANK_ACCOUNT_FAILED = "Your attempt to add bank account with id %s has failed.";
+    public static final String BANK_ACCOUNT_WITH_ID_NOT_FOUND = "Bank account with id %d not found.";
 }
