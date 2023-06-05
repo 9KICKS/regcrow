2816fd191788158c441ecbd497e5af5372eaa5e8
9KICKS <officialjoshua9@gmail.com>
2023-06-05 23:38:34 +0100

    "updated the Regrow app"

---
+++ regcrow/src/main/java/africa/semicolon/regcrow/dtos/request/AdminRegistrationRequest.java
@@ -0,0 +1,11 @@
+package africa.semicolon.regcrow.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminRegistrationRequest {
+    private String email;
+    private String password;
+}
