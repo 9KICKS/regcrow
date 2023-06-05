477bd92d2bf208ca8dbf97c68b26f5ae5c42cd07
9KICKS <officialjoshua9@gmail.com>
2023-06-05 23:40:04 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/models/BankAccount.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/models/BankAccount.java
@@ -1,17 +1,21 @@
 package africa.semicolon.regcrow.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "bank_account")
 public class BankAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,4 +23,17 @@ public class BankAccount {
     private String accountName;
     private String accountNumber;
     private String bankName;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime timeCreated;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void setTimeCreated() {
+        this.timeCreated = LocalDateTime.now();
+    }
 }
