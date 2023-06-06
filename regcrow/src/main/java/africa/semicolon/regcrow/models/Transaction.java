c071e930173923a316103f81268a2d2c75a7ce6d
9KICKS <officialjoshua9@gmail.com>
2023-06-06 23:27:19 +0100

    "updated the Regrow app"

--- regcrow/src/main/java/africa/semicolon/regcrow/models/Transaction.java
+++ regcrow/src/main/java/africa/semicolon/regcrow/models/Transaction.java
@@ -1,5 +1,9 @@
 package africa.semicolon.regcrow.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,4 +25,17 @@ public class Transaction {
     private String description;
     private LocalDateTime createdAt;
     private Status status;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime timeCreated;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void setTimeCreated() {
+        this.timeCreated = LocalDateTime.now();
+    }
 }
